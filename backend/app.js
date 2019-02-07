166d77f6322e9f81fe4c7e36726632adadd0c3f3
Eugene <evgene.vinogradov@mail.ru>
2019-02-07 11:52:49 +0300

    first commit

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+app.get("/",(req,res)=>{
+    res.send("succeful")
+})
+
+app.listen(8080,()=>{
+    console.log("connection completed successfully")
+})
+var s = 1
